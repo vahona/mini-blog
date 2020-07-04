7df43f98ea2080eadb503fa36fede5afdea203e9
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-07-04 10:20:30 +0300

    Modification

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 console.log('it works');
 
 // code your solution in here
+
+// Get access to the DOM
+
+const main = document.getElementsById("post-list");
+const span = document.getElementsByClassName("badge-light");
+const title = document.getElementsByClassName("card-title");
+const paragraph = document.getElementsByClassName("card-text")
+const titleOfNewPost = document.getElementsByTagName('h5');
+const paragraphOfNewPost = document.getElementsByTagName("p");
+paragraphOfNewPost.textContent = "";
+titleOfNewPost.textContent = "";
+
+// Modify the DOM
+// 
+main.addEventListener('click', () =>{
+  
+})
