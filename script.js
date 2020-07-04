f16bbc915e062356d86964c0bf7d6a30cc38fe3c
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-07-04 14:04:12 +0300

    moodification

--- script.js
+++ script.js
@@ -7,14 +7,24 @@ console.log('it works');
 const main = document.getElementsById("post-list");
 const span = document.getElementsByClassName("badge-light");
 const title = document.getElementsByClassName("card-title");
-const paragraph = document.getElementsByClassName("card-text")
-const titleOfNewPost = document.getElementsByTagName('h5');
-const paragraphOfNewPost = document.getElementsByTagName("p");
-paragraphOfNewPost.textContent = "";
-titleOfNewPost.textContent = "";
+const paragraph = document.getElementsByClassName("card-text");
+
 
 // Modify the DOM
 // 
-main.addEventListener('click', () =>{
-  
-})
+
+
+function createNewPost() {
+    let newPost = document.createElement('div');
+    let titleOfNewPost = document.getElementById('new-post-title');
+    let paragraphOfNewPost = document.getElementById("new-post-content");
+    titleOfNewPost.textContent = "";
+    paragraphOfNewPost.textContent = "";
+    div = document.getElementsById("form-group");
+
+    newPost.appendChild(titleOfNewPost);
+    newPost.appendChild(paragraphOfNewPost);
+    newPost.classList.add('post-items-list');
+}
+
+
